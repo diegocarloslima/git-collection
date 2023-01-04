cdd5b482b1a0eb44e0903bd02242c821662905e6
Diego Lima <diegoclal@gmail.com>
2023-01-04 20:39:52 -0300

    Starting to work on build logic

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.diegocarloslima.gitcollection.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+    // TODO
+    }
+}
