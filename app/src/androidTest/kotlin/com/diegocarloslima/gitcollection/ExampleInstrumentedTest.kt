ddc7a82c048b3b40388bf9ae453031f75382cf6e
Diego Lima <diegoclal@gmail.com>
2023-01-10 20:17:36 -0300

    Adding spotless

--- app/src/androidTest/kotlin/com/diegocarloslima/gitcollection/ExampleInstrumentedTest.kt
+++ app/src/androidTest/kotlin/com/diegocarloslima/gitcollection/ExampleInstrumentedTest.kt
@@ -1,12 +1,28 @@
+/*
+ * This file is part of Git Collection.
+ * Copyright (C) 2023-present Diego Carlos Lima <https://diegocarloslima.com/>
+ *
+ * Git Collection is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Git Collection is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.diegocarloslima.gitcollection
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
