8ca6da8467330aa1f87965c163101d6d05132176
Diego Lima <diegoclal@gmail.com>
2023-07-07 18:06:57 -0300

    Dismissing dialog for list setting

--- feature/settings/src/main/kotlin/com/diegocarloslima/gitcollection/feature/settings/ui/SettingsListComponents.kt
+++ feature/settings/src/main/kotlin/com/diegocarloslima/gitcollection/feature/settings/ui/SettingsListComponents.kt
@@ -25,13 +25,16 @@ import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
+import com.diegocarloslima.gitcollection.ui.strings.R as stringsR
 
 @Composable
 internal fun SettingsListSingle(
@@ -82,7 +85,11 @@ private fun SettingsListSingleDialog(
 ) {
     AlertDialog(
         onDismissRequest = onDismissRequest,
-        confirmButton = {},
+        confirmButton = {
+            TextButton(onClick = onDismissRequest) {
+                Text(text = stringResource(id = stringsR.string.action_cancel))
+            }
+        },
         title = { Text(title) },
         text = {
             LazyColumn {
@@ -93,7 +100,10 @@ private fun SettingsListSingleDialog(
                         modifier = Modifier.selectable(
                             selected = selected,
                             role = Role.RadioButton,
-                            onClick = { onEntrySelected(index, item) },
+                            onClick = {
+                                onEntrySelected(index, item)
+                                onDismissRequest()
+                            },
                         ),
                         icon = {
                             RadioButton(
