e87a2572eb0b44b4cc7a65ed601eb18d5a13a81e
Diego Lima <diegoclal@gmail.com>
2023-07-12 19:16:04 -0300

    Starting to work on About screen

---
+++ feature/settings/src/main/kotlin/com/diegocarloslima/gitcollection/feature/settings/ui/AboutScreen.kt
@@ -0,0 +1,27 @@
+/*
+ * This file is part of Git Collection.
+ * Copyright (C) 2023-present Diego Carlos Lima <https://diegocarloslima.com/>
+ *
+ * Git Collection is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Git Collection is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.diegocarloslima.gitcollection.feature.settings.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun AboutScreen() {
+    Text(text = "About screen")
+}
