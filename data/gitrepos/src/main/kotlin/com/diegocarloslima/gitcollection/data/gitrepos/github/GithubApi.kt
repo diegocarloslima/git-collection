65878ecbe323fd6036cb45da1be303d3f0aa07bb
Diego Lima <diegoclal@gmail.com>
2023-08-16 20:27:51 -0300

    Working on GitHub repository search

---
+++ data/gitrepos/src/main/kotlin/com/diegocarloslima/gitcollection/data/gitrepos/github/GithubApi.kt
@@ -0,0 +1,35 @@
+/*
+ * This file is part of Git Collection.
+ * Copyright (C) 2023-present Diego Carlos Lima <https://diegocarloslima.com/>
+ *
+ * Git Collection is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Git Collection is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.diegocarloslima.gitcollection.data.gitrepos.github
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+internal interface GithubApi {
+
+    @GET(value = "search/repositories")
+    suspend fun searchRepositories(
+        @Query("q") query: String,
+        @Query("sort") sort: String,
+        @Query("order") order: String,
+        @Query("per_page") perPage: Int,
+        @Query("page") page: Int,
+    ): Response<GithubRepository>
+}
